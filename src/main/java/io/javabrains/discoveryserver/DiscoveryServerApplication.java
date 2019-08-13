356863e31c5e50da962f74f8a5a01a6bf408a9a3
patidarvjy@gmail.com <patidarvjy@gmail.com>
2019-08-13 14:42:59 +0530

    "First Commit"

---
+++ src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,18 @@
+package io.javabrains.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
+
